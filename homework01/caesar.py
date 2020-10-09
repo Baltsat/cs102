e59b257e564509d1209943223098a1379a196ec2
Konstantin Baltsat <baltsat2002@mail.ru>
2020-10-09 11:03:08 +0300

    Реализованы функции encrypt_caesar() и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,18 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for char in plaintext:
+        if "A" <= char <= "z":
+            symbol = ord(char) + shift % 26
+
+            if "A" <= char <= "Z" < chr(symbol):
+                symbol -= 26
+
+            elif chr(symbol) > "z":
+                symbol -= 26
+
+            char = chr(symbol)
+        ciphertext += char
     return ciphertext
 
 
@@ -33,7 +44,19 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for char in ciphertext:
+        if "A" <= char <= "z":
+            enc = ord(char) - shift % 26
+
+            if enc < ord("A"):
+                enc += 26
+
+            if "a" <= char <= "z" and enc < ord("a"):
+                enc += 26
+
+            char = chr(enc)
+        plaintext += char
+
     return plaintext
 
 
