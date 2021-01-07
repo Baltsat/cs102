1cd203d9db8648441725653b6a9862ea5d4129bc
Konstantin Baltsat <baltsat2002@mail.ru>
2021-01-07 23:49:20 +0500

    Token deleted

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,7 +2,7 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "71d8bf9fddc718b5f51ee256c41d2846edfcf393353556611b524a1a820d0e498113f7315e6edaf6a6952",
+    "access_token": "",
     "version": "5.126",
     "user_id": "213254993",
 }
