86d1db592827ff76554e5ce41ae668064ae96ed2
Konstantin Baltsat <konstantinbaltsat@MacBook-Pro-Konstantin-2.local>
2021-06-25 11:39:17 +0300

    utils used again. reformatted

--- homework07/httpserver/httpserver/__init__.py
+++ homework07/httpserver/httpserver/__init__.py
@@ -1,7 +1,6 @@
 # type: ignore
 
-from .handlers import (BaseHTTPRequestHandler, BaseRequestHandler,
-                       EchoRequestHandler)
+from .handlers import BaseHTTPRequestHandler, BaseRequestHandler, EchoRequestHandler
 from .request import HTTPRequest
 from .response import HTTPResponse
 from .server import HTTPServer, TCPServer
