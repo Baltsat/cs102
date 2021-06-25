66b0e3faac8fd673c1d525699d885c76139126d2
Konstantin Baltsat <konstantinbaltsat@MacBook-Pro-Konstantin-2.local>
2021-06-25 17:37:01 +0300

    static server finished

--- homework07/static_server/static_server.py
+++ homework07/static_server/static_server.py
@@ -1,17 +1,15 @@
 import mimetypes
 import os
 import pathlib
-import time
-import typing as tp
+from email.utils import formatdate
 from urllib.parse import unquote, urlparse
 
-from httpserver import BaseHTTPRequestHandler, HTTPRequest, HTTPResponse, HTTPServer
+from httpserver.httpserver.handlers import BaseHTTPRequestHandler, HTTPResponse, HTTPServer
 
 
 def url_normalize(path: str) -> str:
     if path.startswith("."):
         path = "/" + path
-    # TODO: обработка стандартного порта
     while "../" in path:
         p1 = path.find("/..")
         p2 = path.rfind("/", 0, p1)
@@ -24,10 +22,33 @@ def url_normalize(path: str) -> str:
     return path
 
 
+class FileProducer(object):
+    def __init__(self, file, chunk_size=4096):
+        self.file = file
+        self.chunk_size = chunk_size
+
+    def more(self):
+        if self.file:
+            data = self.file.read(self.chunk_size)
+            if data:
+                return data
+            self.file.close()
+            self.file = None
+        return ""
+
+
 class StaticHTTPRequestHandler(BaseHTTPRequestHandler):
-    def handle_request(self, request: HTTPRequest) -> HTTPResponse:
-        # NOTE: https://tools.ietf.org/html/rfc3986
-        # NOTE: echo -n "GET / HTTP/1.0\r\n\r\n" | nc localhost 5000
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.path: str = ""
+
+    @staticmethod
+    def date_time_string():
+        return formatdate(timeval=None, localtime=False, usegmt=True)
+
+    def send_head(self) -> HTTPResponse:
+        #     NOTE: https://tools.ietf.org/html/rfc3986
+        #     NOTE: echo -n "GET / HTTP/1.0\r\n\r\n" | nc localhost 5000
         _url = urlparse(self._urn)
         self.path, qs = _url.path, _url.query
         self.path = url_normalize(unquote(self.path))
@@ -46,7 +67,7 @@ class StaticHTTPRequestHandler(BaseHTTPRequestHandler):
         return self.response_klass(
             status=200,
             headers={
-                "Server": "My Server",
+                "Server": "My Lab Server",
                 "Date": self.date_time_string(),
                 "Content-Type": str(content_type),
                 "Content-Length": str(content_size),
@@ -54,14 +75,37 @@ class StaticHTTPRequestHandler(BaseHTTPRequestHandler):
             },
         )
 
-    def do_HEAD(self):
-        pass
+    def do_GET(self):
+        try:
+            response = self.send_head()
+        except FileNotFoundError:
+            return self.response_klass(status=404, headers={})
+        except PermissionError:
+            return self.response_klass(status=403, headers={})
+        except:
+            return self.response_klass(status=500, headers={})
+        else:
+            with open(self.path, "rb") as f:
+                producer = FileProducer(f)
+                file = bytes()
+                while chunk := producer.more():
+                    file += chunk
+                response.body = file
+                return response
 
     def do_HEAD(self):
-        pass
+        try:
+            return self.send_head()
+        except FileNotFoundError:
+            return self.response_klass(status=404, headers={})
+        except PermissionError:
+            return self.response_klass(status=403, headers={})
+        except:
+            return self.response_klass(status=500, headers={})
 
 
 class StaticServer(HTTPServer):
+
     def __init__(self, d_root: pathlib.Path, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.document_root: pathlib.Path = d_root
@@ -71,7 +115,7 @@ if __name__ == "__main__":
     document_root = pathlib.Path("static") / "root"
     server = StaticServer(
         timeout=5,
-        document_root=document_root,
+        d_root=document_root,
         port=5000,
         request_handler_cls=StaticHTTPRequestHandler,
     )
