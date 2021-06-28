78e5b2e5240442c62996e6e7985365a56c75e6db
Konstantin Baltsat <konstantinbaltsat@MacBook-Pro-Konstantin-2.local>
2021-06-28 21:02:36 +0300

    ses test

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -5,7 +5,7 @@ from parser import get_news
 
 from bayes import NaiveBayesClassifier
 from bottle import redirect, request, route, run, template
-from db import News, session
+from db import News, Session
 
 
 @tp.no_type_check
